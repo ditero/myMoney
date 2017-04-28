3dc1f540e4df25b901318ada4af32ad7bb6396cb
Anele Sigenu <anelesigenu@gmail.com>
2017-04-28 12:20:27 +0200

    Update myMoney.js

--- javascript/myMoney.js
+++ javascript/myMoney.js
@@ -1,31 +1,42 @@
-var callBackValue = null;
+
+// get the most recent exchange rates via the "live" endpoint:
 function exchangeRates(cb){
-  callBackValue = cb;
+
     var endpoint = 'live'
     var access_key = 'ab595b7e5af131da821709db2f1a2a83';
 
-    // get the most recent exchange rates via the "live" endpoint:
     $.ajax({
         url: 'http://apilayer.net/api/' + endpoint + '?access_key=' + access_key,
         dataType: 'jsonp',
         success: cb
+
   });
 }
-var xeCurrencies = document.getElementById('xeTemplate');//myText
+//HTML Element References
+var xeCurrencies = document.getElementById('xeTemplate');
 var myText = document.getElementById('myText');
+var liveRates = document.getElementById('xeRates');
 var displayResults = document.getElementById('displayResults');
 var listPairs = document.querySelector('.currencies');
 var xeInstance = Handlebars.compile(xeCurrencies.innerHTML);
-var inputValue = 250;
-var totalValue = 0;
+
 var currencies = [];
 
-function genRates(){
 
-}
+
+//Generates Exchange rates fron the API
 exchangeRates(function(rates){
 
+genRates(rates);
 
+//Dynamically add Currency Pairs into the dropdown list:
+rateUSDZAR = rates.quotes.USDZAR;
+listPairs.innerHTML = xeInstance({currencies:currencies});
+
+});
+
+//Add rates to an empty Array.
+function genRates(rates){
   currencies.push ({
     type : 'USDZAR',
     rate : rates.quotes.USDZAR
@@ -38,32 +49,29 @@ exchangeRates(function(rates){
   type : 'USDJPY',
   rate : rates.quotes.USDJPY
 });
-//console.log(rates);
-//Dynamically add Currency Pairs into the dropdown list:
-listPairs.innerHTML = xeInstance({currencies:currencies});
-  //console.log(currencies[0].rate);
-  for (var i = 0; i < currencies.length; i++) {
+displayResults.innerHTML = '$'+currencies[0].rate.toFixed(2);
+}
 
-      console.log(currencies[i].rate);
+//Calculates USD amount with the selected pair.
+function convertToUSD(xePairs, amountUSD){
+  var xePairs = listPairs.value;
+  var amountUSD = myText.value;
+  //Function Call to pass live rates
+exchangeRates(function(rates){
 
+  if (xePairs === 'USDZAR') {
+    displayResults.innerHTML = '$'+(rates.quotes.USDZAR * amountUSD).toFixed(2);
   }
 
-});
-
-console.log(currencies[0]);
-
-
-listPairs.addEventListener('change', function(callBackValue){
-  var valuePair = listPairs.value;
-  //console.log(callBackValue.quotes.valuePair);;
-  if (valuePair === 'USDZAR') {
-    displayResults.innerHTML = 13.00 * myText.value;
+  else if(xePairs === 'USDGBP') {
+    displayResults.innerHTML = '$'+(rates.quotes.USDGBP *amountUSD).toFixed(2);
   }
-
-  else if(valuePair === 'USDGBP') {
-    displayResults.innerHTML = 0.77 * myText.value;
-  }
-  else if(valuePair === 'USDJPY') {
-    displayResults.innerHTML = 110 * myText.value;
+  else if(xePairs === 'USDJPY') {
+    displayResults.innerHTML = '$'+(rates.quotes.USDJPY * amountUSD).toFixed(2);
   }
 });
+
+}
+
+//Automatically Calculates as you type the amount of USD.
+myText.addEventListener('keyup',  convertToUSD);
